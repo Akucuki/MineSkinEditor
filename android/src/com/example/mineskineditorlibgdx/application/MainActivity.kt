094003bb8e01bb3c61e390dd42015b7404904280
Akucuki <pirko.vitaliy@gmail.com>
2023-03-07 16:17:14 +0200

    Performed base setup with Compose + LibGDX

---
+++ android/src/com/example/mineskineditorlibgdx/application/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.mineskineditorlibgdx.application
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication
+import com.example.mineskineditorlibgdx.R
+import com.example.mineskineditorlibgdx.databinding.ActivityMainBinding
+import com.example.mineskineditorlibgdx.utils.NavigationDispatcher
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+typealias onDestinationChanged = NavController.OnDestinationChangedListener
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), AndroidFragmentApplication.Callbacks {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private val destinationChangedListener = onDestinationChanged { _, destination, arguments ->
+        destination.toString()
+        arguments.toString()
+        // TODO place some stuff here
+    }
+
+    @Inject
+    lateinit var navigationDispatcher: NavigationDispatcher
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        lifecycleScope.apply {
+            launch {
+                withContext(Dispatchers.IO) {
+                    val startDestination = provideStartDestination()
+                    withContext(Dispatchers.Main) {
+                        initNavigation(startDestination)
+                    }
+                    launchWhenResumed { observeNavigationCommands() }
+                }
+            }
+        }
+    }
+
+    private fun provideStartDestination(): Int = R.id.fragmentSkinEditor
+
+    private fun initNavigation(startDestination: Int) {
+        (supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment).also { navHost ->
+            val navInflater = navHost.navController.navInflater
+            val navGraph = navInflater.inflate(R.navigation.main_graph)
+            navGraph.setStartDestination(startDestination)
+            navHost.navController.graph = navGraph
+            navController = navHost.navController
+            navController.addOnDestinationChangedListener(destinationChangedListener)
+        }
+    }
+
+    private suspend fun observeNavigationCommands() {
+        for (command in navigationDispatcher.navigationEmitter) command.invoke(navController)
+    }
+
+    override fun exit() {}
+}
---
+++ android/src/com/example/mineskineditorlibgdx/features/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.mineskineditorlibgdx.features.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            Box(
+                modifier = Modifier.fillMaxSize().background(color = Color.Cyan),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "HOME")
+            }
+        }
+    }
+}
