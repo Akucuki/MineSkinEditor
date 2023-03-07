35c19100a83e21c26139d0f7c579915f5574533b
Akucuki <pirko.vitaliy@gmail.com>
2023-03-07 22:50:59 +0200

    Setup of skin editor itself. Added basic model and an example skin

--- core/src/com/example/mineskineditorlibgdx/SkinEditorGame.kt
+++ core/src/com/example/mineskineditorlibgdx/SkinEditorGame.kt
@@ -1,27 +1,85 @@
 package com.example.mineskineditorlibgdx
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.PerspectiveCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g3d.Environment
+import com.badlogic.gdx.graphics.g3d.Model
+import com.badlogic.gdx.graphics.g3d.ModelBatch
+import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.utils.BaseJsonReader
+import com.badlogic.gdx.utils.JsonReader
 import com.badlogic.gdx.utils.ScreenUtils
 
 class SkinEditorGame : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+//    var batch: SpriteBatch? = null
+//    var img: Texture? = null
+    private var modelBatch: ModelBatch? = null
+    private lateinit var environment: Environment
+    private lateinit var cam: Camera
+    private var instance: ModelInstance? = null
+    private lateinit var camController: CameraInputController
+    private lateinit var assets: AssetManager
+    private var loading: Boolean = false
+
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+//        batch = SpriteBatch()
+//        img = Texture("badlogic.jpg")
+
+        modelBatch = ModelBatch()
+        environment = Environment()
+        environment.set(ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f))
+        environment.add(DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f))
+
+        cam = PerspectiveCamera(67f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        cam.position.set(1f, 4f, 1f)
+        cam.lookAt(0f, 0f, 0f)
+        cam.near = 1f
+        cam.far = 300f
+        cam.update()
+
+        camController = CameraInputController(cam)
+        Gdx.input.inputProcessor = camController
+
+        assets = AssetManager()
+        assets.load("humanoid.g3db", Model::class.java)
+        loading = true
+    }
+
+    private fun doneLoading() {
+        val skinModel = assets.get("humanoid.g3db", Model::class.java)
+        instance = ModelInstance(skinModel)
+        loading = false
     }
 
     override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+        if (loading && assets.update()) doneLoading()
+        camController.update()
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+
+        modelBatch?.begin(cam)
+        instance?.let {
+            modelBatch?.render(it, environment)
+        }
+        modelBatch?.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+//        batch!!.dispose()
+//        img!!.dispose()
+        modelBatch?.dispose()
+        assets.dispose()
     }
 }
