df938c06023cffc740c7c8450e455cf9b672a68f
Akucuki <pirko.vitaliy@gmail.com>
2023-03-22 21:59:16 +0200

    Performed some basic refactoring by extracting geometry utility functions to a separate file.

--- core/src/com/example/mineskineditorlibgdx/SkinEditorGame.kt
+++ core/src/com/example/mineskineditorlibgdx/SkinEditorGame.kt
@@ -1,51 +1,24 @@
 package com.example.mineskineditorlibgdx
 
-import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.assets.loaders.TextureLoader
 import com.badlogic.gdx.graphics.*
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.g3d.*
+import com.badlogic.gdx.graphics.g3d.Environment
+import com.badlogic.gdx.graphics.g3d.Model
+import com.badlogic.gdx.graphics.g3d.ModelBatch
+import com.badlogic.gdx.graphics.g3d.ModelInstance
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
-import com.badlogic.gdx.graphics.g3d.model.MeshPart
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.math.*
-import com.badlogic.gdx.math.collision.Ray
+import com.badlogic.gdx.math.Vector3
+import com.example.mineskineditorlibgdx.utils.RaycastGeometry
 
-data class ModelTriangle(
-    val v1: Vector3,
-    val v2: Vector3,
-    val v3: Vector3,
-    val uvs: Array<Vector2>
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ModelTriangle
-
-        if (v1 != other.v1) return false
-        if (v2 != other.v2) return false
-        if (v3 != other.v3) return false
-        if (!uvs.contentEquals(other.uvs)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = v1.hashCode()
-        result = 31 * result + v2.hashCode()
-        result = 31 * result + v3.hashCode()
-        result = 31 * result + uvs.contentHashCode()
-        return result
-    }
-}
-
-class SkinEditorGame : ApplicationAdapter() {
+class SkinEditorGame : Game() {
 
     private var environment: Environment? = null
     private var cam: Camera? = null
@@ -169,10 +142,10 @@ class SkinEditorGame : ApplicationAdapter() {
                     val indices = ShortArray(mesh.numIndices)
                     mesh.getIndices(indices)
 
-                    val triangles = getModelTriangles(indices, meshPart, vertices, mesh)
+                    val triangles = RaycastGeometry.getModelTriangles(indices, meshPart, vertices, mesh)
 
                     val intersectionTriangles = triangles.filter {
-                        intersectRayTriangle(
+                        RaycastGeometry.intersectRayTriangle(
                             ray,
                             it.v1,
                             it.v2,
@@ -181,7 +154,7 @@ class SkinEditorGame : ApplicationAdapter() {
                         )
                     }
                     val nearestIntersectedTriangle = intersectionTriangles.minByOrNull {
-                        cam!!.position.dst(getTriangleCentroid(it))
+                        cam!!.position.dst(RaycastGeometry.getTriangleCentroid(it))
                     }
 
                     if (nearestIntersectedTriangle != null) {
@@ -189,7 +162,7 @@ class SkinEditorGame : ApplicationAdapter() {
                         intersectionsFoundCount++
 
                         val intersectionPoint = Vector3()
-                        intersectRayTriangle(
+                        RaycastGeometry.intersectRayTriangle(
                             ray,
                             nearestIntersectedTriangle.v1,
                             nearestIntersectedTriangle.v2,
@@ -214,7 +187,7 @@ class SkinEditorGame : ApplicationAdapter() {
                             instance!!.materials.first()
                                 .get(TextureAttribute.Diffuse) as TextureAttribute
                         val texture = instanceTextureAttribute.textureDescription.texture
-                        val uv = getIntersectionUV(intersectionPoint, nearestIntersectedTriangle)
+                        val uv = RaycastGeometry.getIntersectionUV(intersectionPoint, nearestIntersectedTriangle)
                         val textureX = (uv.x * texture.width).toInt()
                         val textureY = (uv.y * texture.height).toInt()
 
@@ -245,126 +218,6 @@ class SkinEditorGame : ApplicationAdapter() {
     }
 }
 
-private fun getModelTriangles(
-    indices: ShortArray,
-    meshPart: MeshPart,
-    vertices: FloatArray,
-    mesh: Mesh
-): List<ModelTriangle> {
-    val triangles = mutableListOf<ModelTriangle>()
-    for (i in indices.indices step 3) {
-        val stride = meshPart.mesh.vertexSize / 4
-        val vertexIndex1 = indices[i].toInt() * stride
-        val vertexIndex2 = indices[i + 1].toInt() * stride
-        val vertexIndex3 = indices[i + 2].toInt() * stride
-
-        val vertex1 = Vector3(
-            vertices[vertexIndex1],
-            vertices[vertexIndex1 + 1],
-            vertices[vertexIndex1 + 2]
-        )
-        val vertex2 = Vector3(
-            vertices[vertexIndex2],
-            vertices[vertexIndex2 + 1],
-            vertices[vertexIndex2 + 2]
-        )
-        val vertex3 = Vector3(
-            vertices[vertexIndex3],
-            vertices[vertexIndex3 + 1],
-            vertices[vertexIndex3 + 2]
-        )
-
-        val vertexAttributes = mesh.vertexAttributes
-        val uvOffset =
-            vertexAttributes.findByUsage(VertexAttributes.Usage.TextureCoordinates).offset / 4
-
-        val uv1 = Vector2(
-            vertices[vertexIndex1 + uvOffset],
-            vertices[vertexIndex1 + uvOffset + 1]
-        )
-        val uv2 = Vector2(
-            vertices[vertexIndex2 + uvOffset],
-            vertices[vertexIndex2 + uvOffset + 1]
-        )
-        val uv3 = Vector2(
-            vertices[vertexIndex3 + uvOffset],
-            vertices[vertexIndex3 + uvOffset + 1]
-        )
-
-        val uvs = arrayOf(uv1, uv2, uv3)
-
-        val triangle = ModelTriangle(vertex1, vertex2, vertex3, uvs)
-        triangles.add(triangle)
-    }
-    return triangles
-}
-
-private fun calculateBarycoord3D(
-    point: Vector3,
-    a: Vector3,
-    b: Vector3,
-    c: Vector3
-): Vector3 {
-    val v0 = Vector3().set(b).sub(a)
-    val v1 = Vector3().set(c).sub(a)
-    val v2 = Vector3().set(point).sub(a)
-
-    val d00 = v0.dot(v0)
-    val d01 = v0.dot(v1)
-    val d11 = v1.dot(v1)
-    val d20 = v2.dot(v0)
-    val d21 = v2.dot(v1)
-
-    val denom = d00 * d11 - d01 * d01
-    val barycentricOut = Vector3()
-    barycentricOut.x = (d11 * d20 - d01 * d21) / denom
-    barycentricOut.y = (d00 * d21 - d01 * d20) / denom
-    barycentricOut.z = 1.0f - barycentricOut.x - barycentricOut.y
-
-    return barycentricOut
-}
-
-private fun getTriangleCentroid(triangle: ModelTriangle): Vector3 {
-    val centroid = Vector3()
-    centroid.set(triangle.v1)
-    centroid.add(triangle.v2)
-    centroid.add(triangle.v3)
-    centroid.scl(1f / 3f)
-    return centroid
-}
-
-private fun getIntersectionUV(
-    intersectionPoint: Vector3,
-    triangle: ModelTriangle
-): Vector2 {
-    val barycentricCoords = calculateBarycoord3D(
-        intersectionPoint,
-        triangle.v1,
-        triangle.v2,
-        triangle.v3
-    )
-
-    require(
-        GeometryUtils.barycoordInsideTriangle(
-            Vector2(
-                barycentricCoords.x,
-                barycentricCoords.y
-            )
-        )
-    ) {
-        "Barycentric coordinates are outside the triangle!"
-    }
-
-    val uv1 = triangle.uvs[0]
-    val uv2 = triangle.uvs[1]
-    val uv3 = triangle.uvs[2]
-
-    return Vector2(
-        barycentricCoords.z * uv1.x + barycentricCoords.x * uv2.x + barycentricCoords.y * uv3.x,
-        barycentricCoords.z * uv1.y + barycentricCoords.x * uv2.y + barycentricCoords.y * uv3.y
-    )
-}
-
 private fun drawVertices(
     vertices: FloatArray,
     vertexSize: Int,
@@ -380,76 +233,4 @@ private fun drawVertices(
         shapeRenderer.box(x, y, z, .4f, .4f, .4f)
     }
     shapeRenderer.end()
-}
-
-private fun intersectRayTriangle(
-    ray: Ray,
-    t1: Vector3,
-    t2: Vector3,
-    t3: Vector3,
-    intersection: Vector3?
-): Boolean {
-    val v0 = Vector3()
-    val v1 = Vector3()
-    val v2 = Vector3()
-    val p = Plane(Vector3(), 0f)
-    val i = Vector3()
-
-    val edge1 = v0.set(t2).sub(t1)
-    val edge2 = v1.set(t3).sub(t1)
-
-    val pvec = v2.set(ray.direction).crs(edge2)
-    var det = edge1.dot(pvec)
-    if (MathUtils.isZero(det)) {
-        p.set(t1, t2, t3)
-        if (p.testPoint(ray.origin) == Plane.PlaneSide.OnPlane && safeIsPointInTriangle(
-                ray.origin,
-                t1,
-                t2,
-                t3
-            )
-        ) {
-            intersection?.set(ray.origin)
-            return true
-        }
-        return false
-    }
-
-    det = 1f / det
-    val tvec = i.set(ray.origin).sub(t1)
-    val u = tvec.dot(pvec) * det
-    if (u < 0f || u > 1f) return false
-
-    val qvec = tvec.crs(edge1)
-    val v = ray.direction.dot(qvec) * det
-    if (v < 0f || u + v > 1f) return false
-
-    val t = edge2.dot(qvec) * det
-    if (t < 0f) return false
-
-    if (intersection != null) {
-        if (t <= MathUtils.FLOAT_ROUNDING_ERROR) {
-            intersection.set(ray.origin)
-        } else {
-            ray.getEndPoint(intersection, t)
-        }
-    }
-    return true
-}
-
-private fun safeIsPointInTriangle(point: Vector3, t1: Vector3, t2: Vector3, t3: Vector3): Boolean {
-    val v0 = Vector3()
-    val v1 = Vector3()
-    val v2 = Vector3()
-
-    v0.set(t1).sub(point)
-    v1.set(t2).sub(point)
-    v2.set(t3).sub(point)
-
-    v1.crs(v2)
-    v2.crs(v0)
-
-    if (v1.dot(v2) < 0f) return false
-    v0.crs(v2.set(t2).sub(point))
-    return v1.dot(v0) >= 0f
 }
